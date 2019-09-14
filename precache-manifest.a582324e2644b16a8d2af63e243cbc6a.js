c8b2c352e3addb79d757220a47298e59708cf7dd
Julian <dev.julian.kowalczuk@gmail.com>
2019-09-14 04:18:28 +0200

    sunscrapers-stock-tracker

---
+++ precache-manifest.a582324e2644b16a8d2af63e243cbc6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02de1d5ca1fb0a5d7bf2e2dc72bdf3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a52f26cbcc69629767",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ab7f82c9c9eb929cc6cc",
+    "url": "/static/css/main.b164e03d.chunk.css"
+  },
+  {
+    "revision": "00a52f26cbcc69629767",
+    "url": "/static/js/2.e0225746.chunk.js"
+  },
+  {
+    "revision": "ab7f82c9c9eb929cc6cc",
+    "url": "/static/js/main.c773a9d7.chunk.js"
+  },
+  {
+    "revision": "c71d259bc30e1b72e925",
+    "url": "/static/js/runtime~main.57a0ee4d.js"
+  }
+]);
